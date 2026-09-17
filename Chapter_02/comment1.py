2492a5204a73739106b1c647fd506500446fdbe1
rtelesko <rainer.telesko@fhnw.ch>
2026-09-17 07:53:57 +0200

    Update comment1.py

--- Chapter_02/comment1.py
+++ Chapter_02/comment1.py
@@ -1,5 +1,11 @@
-# This program displays a person's
-# name and address.
+# Single-line comment
+# This program displays a person's name and address.
 print('Kate Austen')
 print('123 Full Circle Drive')
 print('Asheville, NC 28899')
+
+# Multi-line comment
+"""
+This looks like a
+multiline comment.
+"""
