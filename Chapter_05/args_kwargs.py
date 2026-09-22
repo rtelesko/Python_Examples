b492de9f3cf710eefeb011881700bae1fe05eda9
rtelesko <rainer.telesko@fhnw.ch>
2026-09-22 17:44:19 +0200

    Updating args_kwargs.py

--- Chapter_05/args_kwargs.py
+++ Chapter_05/args_kwargs.py
@@ -1,6 +1,7 @@
 # Source: https://www.freecodecamp.org/news/args-and-kwargs-in-python/ (adapted)
 # How to Use *args in Python
 # *args allows us to pass a variable number of non-keyword arguments to a Python function
+# *numbers packs all positional arguments into a tuple named numbers
 def add(*numbers):
     total = 0
     for num in numbers:
@@ -19,7 +20,6 @@ print()
 
 # How to Use **kwargs in Python
 # **kwargs allows us to pass a variable number of keyword arguments to a Python function
-
 def total_fruits(**fruits):
     total = 0
     for amount in fruits.values():
