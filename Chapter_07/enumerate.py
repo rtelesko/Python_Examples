0a7ff77aac1de6f68e918c58ea0df65e6e281f88
rtelesko <rainer.telesko@fhnw.ch>
2026-09-23 11:17:56 +0200

    Updating enumerate.py

--- Chapter_07/enumerate.py
+++ Chapter_07/enumerate.py
@@ -1,7 +1,7 @@
 # Using enumerate() to list
 courses = ['Java', 'Python', 'Pandas', 'Sparks']
 new_list = enumerate(courses)
-print(list(new_list))
+print(list(new_list))       # Converting enumerator object into list
 
 # Using enumerate() and for loop
 for index, value in enumerate(courses):
